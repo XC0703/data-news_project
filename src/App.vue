47b6fc2e86f519cf44a01c2d4bcfc256574ca3e9
明远湖之鱼 <488548816@qq.com>
2022-08-10 22:56:01 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <full-page :options="options">
+          <!-- 每个需要全屏滚动的页面都需要包在<div class="section"></div>中 -->
+          <div 
+            class="section" 
+            v-for="index in 10" 
+            :style="{background: 'url(/static/images/bg' + index + '.jpg)'}"
+            :key="index"
+          >
+              section{{index}}
+              <div 
+                class="section1"
+                v-if="index === 1"
+              >		
+              </div>
+          </div>
+        </full-page>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+        options:{
+            licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+            afterLoad: this.afterLoad,
+            autoScrolling: false,
+            scrollOverflow: true,
+            scrollBar: false,
+            menu: '#menu',
+        }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.section{
+	background-repeat: no-repeat !important;
+	background-size: cover !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 引入vue-fullpage
+import 'vue-fullpage.js/dist/style.css'
+import VueFullPage from 'vue-fullpage.js'
+
+createApp(App).use(VueFullPage).mount('#app')
