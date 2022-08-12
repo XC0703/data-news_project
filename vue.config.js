0887170fc801df3471067e3967f2d6748ff9ece3
明远湖之鱼 <488548816@qq.com>
2022-08-12 20:33:26 +0800

    地图板块完成

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    host: '0.0.0.0',
+  // https:true,
+    port: 8080,
+    client: {
+      webSocketURL: 'ws://0.0.0.0:8080/ws',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  },
   lintOnSave:false,  // 修改成false 就是不检查了
   transpileDependencies: true
 })
